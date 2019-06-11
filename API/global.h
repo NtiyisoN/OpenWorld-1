16283ef68b6607e9a488dfe296a7efb4d925d844
Blake <contact@blake-cooper.com>
2019-06-11 15:32:28 -0400

    Space headers

--- API/global.h
+++ API/global.h
@@ -3,18 +3,29 @@
  *
  * global.h 
  * created:	2019-05-10 
- * updated:	2019-06-10 
+ * updated:	2019-06-11 
  * 
  * "Ever tried. Ever failed. No matter. Try again. Fail again. Fail better."
  * ~Samuel Beckett
  */
 
-#define MAX_CHARS_FOR_DESCRIPTION 2048
-#define FILENAME_MAX_LENGTH 25
-
-char *describe(char *object);
-
 /* CUSTOMIZEABLE VARIABLES: */
+
+/* Global: */
+
+typedef unsigned short int bool
+
+#define FALSE 0
+#define TRUE 1
+
+#define MAX_CHARS_FOR_DESCRIPTION 2048
+#define FILENAME_MAX_LENGTH 32
+#define MAX_CHARS_IN_NAME 32
+
+#define NORTH 0
+#define SOUTH 1
+#define EAST 2
+#define WEST 3
 
 /* Language: */
 //TODO
@@ -26,3 +37,29 @@ char *describe(char *object);
 
 //How long do historical eras last?
 #define YEARS_PER_HISTORICAL_ERA 2000
+
+/* Space: */
+
+/* Type of site: */
+#define UNIVERSE 0
+#define GALAXY 1
+#define SYSTEM 2
+#define PLANET 3
+#define ANOMALY 4
+#define REGION 5
+#define COUNTRY 6
+#define SETTLEMENT 7
+#define SITE 8
+#define STRUCTURE 9
+#define ROOM 10
+
+/* Types of biomes: */
+#define GRASS 0
+#define SAVANA 1
+#define DESERT 2
+#define TUNDRA 3
+#define TAIGA 4
+#define FOREST 5
+#define SWAMP 6
+#define BEACH 7
+#define OCEAN 8
---
+++ API/space.h
@@ -0,0 +1,101 @@
+/* OpenWorld 1.0 
+ * by Blake Cooper
+ *
+ * space.h 
+ * created:	2019-06-11 
+ * updated:	2019-06-11 
+ * 
+ */
+
+#include "global.h"
+#include "creature.c"
+
+/* Definitions: */
+
+/* Interior/Exterior: */
+#define EXTERIOR 0
+#define INTERIOR 1
+
+/* Structs: */
+typedef struct site {
+	bool has_name;
+	char name[MAX_CHARS_IN_NAME];
+	
+	bool has_type;
+	short type;
+
+	bool has_climate;
+	bool inherits_climate_from_parent;
+	short climate;
+
+	bool has_creator;
+	Creature *creator;
+
+	bool has_owner;
+	Creature *owner;
+
+	bool is_governed;
+	Creature *governor;
+
+	bool interior_or_exterior;
+
+	Site *to_north;
+	Site *to_south;
+	Site *to_east;
+	Site *to_west;
+
+	Map *location;
+	Map *map;
+	
+} Site;
+
+typedef struct map {
+	Site *of_site;
+	Site *capital;
+} map;
+
+//
+//functions
+Site *makeSpaceFromFile(FILE filename[FILENAME_MAX_LENGTH]);
+
+Site *makeSpaceWithContext(OpenWorld world);
+
+Site *makeRandomSpace();
+
+Site *makeRandomSite();
+
+Site *makeSiteOfType(short type);
+
+void moveSiteTo(Site location);
+
+void situateSite(short direction, Site location);
+
+/* Getters: */
+char *getSiteName(const Site site);
+
+short getSiteType(const Site site);
+
+short getSiteClimate(const Site site);
+
+Creature *getSiteOwner(const Site site);
+
+Creature *getSiteGovernor(const Site site);
+
+Site *getSiteNorthOf(const Site site);
+
+Site *getSiteSouthOf(const Site site);
+
+Site *getSiteWestOf(const Site site);
+
+Site *getSiteEastOf(const Site site);
+
+/* Setters: */
+void setSiteName(Site site, char name[MAX_CHARS_IN_NAME]);
+
+void setSiteType(Site site, short type);
+
+void setSiteClimate(Site site, short climate);
+
+void setSiteOwner(Site site, Creature *owner);
+
+void setSiteGovernor(Site site, Creature *governor);
